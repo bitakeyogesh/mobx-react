c5fc1897d00b71ca6987ca2cd787fd66b4dfffbe
a3znzg <yogesh.bitake@siemens.com>
2021-02-19 22:26:08 +0530

    enhance newnoteform component

--- src/NewNoteForm.js
+++ src/NewNoteForm.js
@@ -4,10 +4,16 @@ import {useNotesStore} from './NotesContext';
 export const NewNoteForm = ()=>{
     const store = useNotesStore();
     const [noteText, setNoteText] = useState("");
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        store.addNote(noteText)
+        setNoteText("");
+    }
     return (
-        <>
+        <form onSubmit={handleSubmit}>
             <input  type="text" value={noteText} onChange={(e)=>setNoteText(e.target.value)}></input>
-            <button onClick={()=>store.addNote(noteText)}>Add Note</button>
-        </>
+            <button type="submit">Add Note</button>
+        </form>
     )
 }

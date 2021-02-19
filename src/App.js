7b2422e2342f7b502f457a4ddb49eb98207a56fc
a3znzg <yogesh.bitake@siemens.com>
2021-02-19 22:40:47 +0530

    some styling

--- src/App.js
+++ src/App.js
@@ -7,15 +7,17 @@ import { observer } from 'mobx-react-lite';
 const App = observer((props)=>{
   const store = useNotesStore();
   return (
-    <>
+    <div className="App-header">
+      <h1>Add/Remove Task </h1>
+      <NewNoteForm/>
+
       <ul>
           {store.notes.map((note)=>(
             <li onClick={()=> store.removeNote(note.id)} key={note.id}>{note.text}</li>
           )
         )}
       </ul>
-      <NewNoteForm/>
-    </>
+    </div>
   )
 })
 
